6bbd13f9fdf8397dcede04bc28779700f7135aa8
haohuisss <71999497+haohuisss@users.noreply.github.com>
2023-01-30 20:03:32 +0800

    Add files via upload

---
+++ args_xq.py
@@ -0,0 +1,32 @@
+import os
+import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+file_path = os.path.dirname(__file__)
+
+model_dir = os.path.join(file_path, 'ModelParams/chinese_L-12_H-768_A-12/')
+config_name = os.path.join(model_dir, 'bert_config.json')
+ckpt_name = os.path.join(model_dir, 'bert_model.ckpt')
+output_dir = os.path.join(file_path, 'Output/XQ/bert_result/')
+vocab_file = os.path.join(model_dir, 'vocab.txt')
+data_dir = os.path.join(file_path, 'Data/XQ_Data/')
+
+num_train_epochs = 3
+batch_size = 32
+learning_rate = 0.00005
+
+# gpu使用率
+gpu_memory_fraction = 1
+
+# 默认取倒数第二层的输出值作为句向量
+layer_indexes = [-2]
+
+# 序列的最大程度，单文本建议把该值调小
+max_seq_len = 60
+
+# 预训练模型
+train = True
+
+# 测试模型
+test = False
